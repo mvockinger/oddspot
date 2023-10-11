2df101a56845ef2e396b3f97d1ddad34d6f4ff7b
mvockinger <m.vockinger@googlemail.com>
2023-10-11 16:16:19 +0200

    New Style for Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,10 +7,11 @@ const Navbar = () => {
       <button className='home-button'></button>
     </div>
     <div className='navbar-items-container'>
-    <a className='navbar-a'><i class="fa-solid fa-book"></i> Blog </a>
-      <a className='navbar-a'><i class="fa-regular fa-calendar-check"></i> Daily Picks </a>
-      <a className='navbar-a'><i class="fa-solid fa-chart-column"></i> Statistics </a>
-      <a className='navbar-a'><i class="fa-solid fa-tag"></i> Pricing </a>
+      <a className='navbar-a'>Community <i class="fa-solid fa-users"></i></a>
+      <a className='navbar-a'>Daily Picks <i class="fa-regular fa-calendar-check"></i></a>
+      <a className='navbar-a'>Predictions <i class="fa-solid fa-bullseye"></i></a>
+      <a className='navbar-a'>Statistics <i class="fa-solid fa-chart-column"></i></a>
+      <a className='navbar-a'>Pricing <i class="fa-solid fa-tag"></i></a>
     </div>
     <div className='navbar-login-container'>
     <button className='login-button'><i class="fa-solid fa-user-tie"></i> Login </button>
