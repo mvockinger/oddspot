00021566f27dcd4f7e3128aca5ef90e4f93481cd
mvockinger <m.vockinger@googlemail.com>
2023-10-12 22:05:54 +0200

    Navbar.css-file, 2 dropdowns, better icons

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,36 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons';
-
+import './Navbar.css';
 
 const Navbar = () => {
-  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const [isDropdownVisibleSport, setDropdownVisibleSport] = useState(false);
+  const [isDropdownVisibleCommunity, setDropdownVisibleCommunity] = useState(false);
   const [isClickInside, setClickInside] = useState(false);
-  const dropdownRef = useRef(null);
+  const dropdownRefSport = useRef(null);
+  const dropdownRefCommunity = useRef(null);
 
-  const handleDropdownToggle = () => {
-    setDropdownVisible(!isDropdownVisible);
+  const handleDropdownToggleSport = () => {
+    setDropdownVisibleSport(!isDropdownVisibleSport);
+    setDropdownVisibleCommunity(false);
+    setClickInside(true);
+  };
+
+  const handleDropdownToggleCommunity = () => {
+    setDropdownVisibleCommunity(!isDropdownVisibleCommunity);
+    setDropdownVisibleSport(false);
     setClickInside(true);
   };
 
   const handleClickOutside = (event) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setDropdownVisible(false);
+    if (
+      dropdownRefSport.current &&
+      !dropdownRefSport.current.contains(event.target) &&
+      dropdownRefCommunity.current &&
+      !dropdownRefCommunity.current.contains(event.target)
+    ) {
+      setDropdownVisibleSport(false);
+      setDropdownVisibleCommunity(false);
       setClickInside(false);
     }
   };
@@ -26,42 +41,75 @@ const Navbar = () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
-  
-  return <nav className='navbar-container'>
-    <div className='nav-logo-container'>
-      <button className='home-button'></button>
-    </div>
-    <div className='navbar-items-container'>
-      <a className='navbar-item'>upcoming events <i class="fa-regular fa-calendar"></i></a>
-      <a className='navbar-item'>free picks <i class="fa-regular fa-circle-check"></i></a>
-      <a className='navbar-item'>predictions <i class="fa-solid fa-bullseye"></i></a>
-      <a className='navbar-item'>statistics <i class="fa-solid fa-chart-column"></i></a>
-      <a className='navbar-item'>community <i class="fa-solid fa-users"></i></a>
-      <div
-        className={`dropdown ${isDropdownVisible || isClickInside ? 'active' : ''}`}
-        onClick={handleDropdownToggle}
-        ref={dropdownRef}
-      >
-        <a href="#" className="navbar-item">sport <FontAwesomeIcon icon={isDropdownVisible ? faCaretUp : faCaretDown} className="arrow-icon" /></a>
-        {isDropdownVisible && (
-          <div className="dropdown-content">
-            <a href="#"><i class="fa-regular fa-futbol"></i> football</a>
-            <a href="#"><i class="fa-solid fa-basketball"></i> basketball</a>
-            <a href="#"><i class="fa-solid fa-baseball"></i> tennis</a>
-            <a href="#"><i class="fa-solid fa-hockey-puck"></i> ice hockey</a>
-            <a href="#"><i class="fa-solid fa-volleyball"></i> volleyball</a>
-            <a href="#"><i class="fa-solid fa-football"></i> american football</a>
-          </div>
-        )}
+
+  return (
+    <nav className='navbar-container'>
+      <div className='nav-logo-container'>
+        <button className='home-button'></button>
       </div>
-      <a className='navbar-item'>pricing <i class="fa-solid fa-tag"></i></a>
-    </div>
-    <div className='navbar-login-container'>
-    <button className='navbar-button sign-up-button'> Sign up </button>
-    <button className='navbar-button login-button'> Login </button>
-    </div>
-  </nav>;
+      <div className='navbar-items-container'>
+        <a className='navbar-item'>
+          <i className="fa-brands fa-get-pocket"></i>free picks
+        </a>
+        <a className='navbar-item'>
+          <i className="fa-regular fa-calendar"></i>upcoming
+        </a>
+        <a className='navbar-item'>
+          <i className="fa-solid fa-chart-column"></i>stats
+        </a>
+        <div
+          className={`dropdown ${isDropdownVisibleSport || isClickInside ? 'active' : ''}`}
+          onClick={handleDropdownToggleSport}
+          ref={dropdownRefSport}
+        >
+          <a href="#" className="navbar-item">
+            <i className="fa-solid fa-bullseye"></i> sport{' '}
+            <FontAwesomeIcon icon={isDropdownVisibleSport ? faCaretUp : faCaretDown} className="arrow-icon" />
+          </a>
+          {isDropdownVisibleSport && (
+            <div className="dropdown-content">
+              {
+                <div className="dropdown-content">
+                  <a href="#"><i className="fa-regular fa-futbol"></i>football</a>
+                  <a href="#"><i className="fa-solid fa-basketball"></i>basketball</a>
+                  <a href="#"><i className="fa-solid fa-baseball"></i>tennis</a>
+                  <a href="#"><i className="fa-solid fa-football"></i>american football</a>
+                  <a href="#"><i className="fa-solid fa-hockey-puck"></i>ice hockey</a>
+                  <a href="#"><i className="fa-solid fa-volleyball"></i>volleyball</a>
+                  <a href="#"><i className="fa-solid fa-baseball-bat-ball"></i>baseball</a>
+                </div>
+              }
+            </div>
+          )}
+        </div>
+        <div
+          className={`dropdown ${isDropdownVisibleCommunity || isClickInside ? 'active' : ''}`}
+          onClick={handleDropdownToggleCommunity}
+          ref={dropdownRefCommunity}
+        >
+          <a href="#" className="navbar-item">
+            <i className="fa-regular fa-comments"></i>community{' '}
+            <FontAwesomeIcon icon={isDropdownVisibleCommunity ? faCaretUp : faCaretDown} className="arrow-icon" />
+          </a>
+          {isDropdownVisibleCommunity && (
+            <div className="dropdown-content">
+              {
+                <div className="dropdown-content">
+                  <a href="#"><i class="fa-solid fa-globe"></i>forum</a>
+                  <a href="#"><i class="fa-solid fa-comment-dots"></i>community picks</a>
+                  <a href="#"><i class="fa-solid fa-chess"></i>strategies</a>
+                </div>
+              }
+            </div>
+          )}
+        </div>
+      </div>
+      <div className='navbar-login-container'>
+        <button className='navbar-button sign-up-button'>Sign up</button>
+        <button className='navbar-button login-button'>Login</button>
+      </div>
+    </nav>
+  );
+};
 
-}
-
-export default Navbar
+export default Navbar;
